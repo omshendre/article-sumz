317573a419892b4869c3029e3a8e831791b73a5a
omshendre <omshendre725@gmail.com>
2024-06-02 11:24:04 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+// import React from 'react';
+// import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, } from '@fortawesome/free-brands-svg-icons';
+const Footer = () => {
+  return (
+    <footer className="footer">
+      
+        <div className="footer-text">© {new Date().getFullYear()} Om Shendre</div>
+        <div className="footer-links">
+          <a
+            href="https://www.linkedin.com/in/omshendre/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a
+            href="https://github.com/omshendre"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer-link"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+     
+    </footer>
+  );
+};
+
+export default Footer;
